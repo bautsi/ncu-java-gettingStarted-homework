b84c9a094a8d4702583c791e25ca29282e94980e
bautsi <b3r1a3dtw@gmail.com>
2022-05-25 14:34:22 +0800

    a

--- Test.java
+++ Test.java
@@ -1,10 +1,10 @@
 public class Test{
     public static void main(String [] args){
         A a=new A();
-        //B b=a;
+        //B b=a; //用不到了
         C c=a;
         
         c.m2();
-        //b.m3();
+        //b.m3(); //因C沒有m3()所以不能DMB
     }
 }
