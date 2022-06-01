7e7f7a06b840f278d00b4c2332d1aa90d45c233e
bautsi <b3r1a3dtw@gmail.com>
2022-06-01 16:08:55 +0800

    a

--- Test.java
+++ Test.java
@@ -10,3 +10,7 @@ public class Test{
         a.md();
     }
 }
+
+// 1.An interface has only (1)abstract methods (2)constants
+//  (1) 介面裡的方法都默認是公開且抽象
+//  (2) 介面裡的變數一定是常數且默認是公開、實體和不可被改寫的
--- Test1.java
+++ Test1.java
@@ -8,3 +8,10 @@ public class Test1{
         //b.m3(); //因C沒有m3()所以不能DMB
     }
 }
+
+//1.DMB作用的條件：DMB works only for the common methods that are
+//　 available in both super and sub@ (super跟sub都
+//   有的同一支方法才行)
+
+//2.子類別何情況需要宣告成抽象：當父類別為抽象且此子類別沒有要
+//                            implement(實作)時
