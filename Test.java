0964d955129ea530d943615125170ad6adca4e36
bautsi <b3r1a3dtw@gmail.com>
2022-06-01 13:15:36 +0800

    a

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test{
+    public static void main(String [] args){
+        C c=new C();
+        A a=new A();
+
+        c.ma();
+        c.mb();
+        a.ma();
+        a.mb();
+        a.md();
+    }
+}
